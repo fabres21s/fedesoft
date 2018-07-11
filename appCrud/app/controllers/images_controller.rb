e5e453a9fc96cd1df049e41ed00bd8f1444136d0
SALA COMPUTO 310 <SALA310@universidad.ean>
2018-07-10 19:38:52 -0500

    Editar

--- appCrud/app/controllers/images_controller.rb
+++ appCrud/app/controllers/images_controller.rb
@@ -1,5 +1,7 @@
 class ImagesController < ApplicationController
     
+    before_action :set_image, only: [:show, :edit, :update, :destroy]
+    
     def new
         @image = Image.new
     end
@@ -9,17 +11,31 @@ class ImagesController < ApplicationController
     end
     
     def create
-        
         @image  = Image.new images_params
         @image.save
-        
-        #redirect_to @image
+        redirect_to @image
     end
     
     def show
+    end
+    
+    def set_image
         @image = Image.find params[:id]
     end
     
+    def edit
+    end
+    
+    def update
+        @image.update images_params
+        redirect_to @image
+    end
+    
+    
+    def destroy
+        @image.destroy
+        redirect_to @image
+    end
     
     private 
     def images_params
